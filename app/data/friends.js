9bf13c42866e5269171db074ccdd87e7a9671021
MPA <50469825+MPA120@users.noreply.github.com>
2019-09-30 19:33:52 -0400

    friends js and survey html initial

---
+++ app/data/friends.js
@@ -0,0 +1,40 @@
+//Starting with dummy data
+
+var friendList = [
+    {
+        name: "Elmo",
+        profilepic: "https://vignette.wikia.nocookie.net/muppet/images/5/5a/Elmo-elmo-elmo.jpg/revision/latest?cb=20110917000614",
+        scores: [
+            "4",
+            "5",
+            "5",
+            "2",
+            "4",
+            "2",
+            "1",
+            "5",
+            "3",
+            "3",
+
+        ]
+    },
+
+    {
+        name: "Cookie Monster",
+        profilePic: "https://cnet3.cbsistatic.com/img/YuLvQxpo04T02vD5Zp-Cogdim2g=/1200x675/center/top/2019/07/26/26589aa5-6aee-48be-bb2a-2505e411d834/cookie.jpg",
+        scores: [
+            "3",
+            "1",
+            "2",
+            "4",
+            "1",
+            "1",
+            "3",
+            "2",
+            "4",
+            "5",
+
+    
+        ]
+    }
+]
